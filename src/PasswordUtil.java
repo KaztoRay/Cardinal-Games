e194e6ec30d9f85b8b93bcdd49041c4229edf980
KaztoRay <dsph9245@naver.com>
2023-10-05 14:00:00 +0900

    feat: 비밀번호 해싱 유틸리티 추가
    - SHA-256 기반 비밀번호 해싱
    - 비밀번호 검증 메서드 추가
    - 평문 비밀번호 저장 방지

---
+++ src/PasswordUtil.java
@@ -0,0 +1,38 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * 비밀번호 해싱 유틸리티 클래스
+ * SHA-256 해싱을 사용하여 비밀번호를 안전하게 저장
+ */
+public class PasswordUtil {
+    
+    /**
+     * 비밀번호를 SHA-256으로 해싱
+     * @param password 원본 비밀번호
+     * @return 해싱된 비밀번호 (Base64 인코딩)
+     */
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("[PasswordUtil] 해싱 실패: " + e.getMessage());
+            return password; // 해싱 실패 시 원본 반환 (fallback)
+        }
+    }
+    
+    /**
+     * 비밀번호 검증
+     * @param inputPassword 입력된 비밀번호
+     * @param storedHash 저장된 해시
+     * @return 일치 여부
+     */
+    public static boolean verifyPassword(String inputPassword, String storedHash) {
+        String inputHash = hashPassword(inputPassword);
+        return inputHash.equals(storedHash);
+    }
+}
